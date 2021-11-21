3c1d8a257edd701839a2f5913e0fce8e0b4f2e16
Ugo <ugo.prenat@gmail.com>
2021-11-21 18:20:40 +0100

    start 404 page

--- client/src/components/pages/PageNotFound.jsx
+++ client/src/components/pages/PageNotFound.jsx
@@ -7,8 +7,12 @@ export default function PageNotFound() {
 
     return (
         <div className="page-not-found-container component">
-            <h1>404 not found</h1>
-            <button onClick={() => history.push('/')}>Revenir au site</button>
+            <div className="content-container">
+                <img src="" alt="" />
+                <p className="title">Oups...</p>
+                <p className="text">Il semble que cette page n'existe pas</p>
+                <button onClick={() => history.push('/')}>Retour au site</button>
+            </div>
         </div>
     )
 }
