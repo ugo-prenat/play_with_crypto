3ab0fbb071a7c463a541a940bd4d0ee63795469c
Ugo <ugo.prenat@gmail.com>
2022-01-07 17:56:59 +0100

    indication on website dev version

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,3 +1,5 @@
 const BACKEND_URL = 'https://play-with-crypto-api.herokuapp.com'
+//const APP_DOMAIN = 'play-with-crypto.fr'
+const APP_DOMAIN = 'localhost'
 
-export { BACKEND_URL }
+export { BACKEND_URL, APP_DOMAIN }
