0e9ce53a422bfdfb0a453982cec4b57071544161
Ugo <ugo.prenat@gmail.com>
2022-01-07 17:58:27 +0100

    forgot that in the indication on website dev version

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,5 +1,4 @@
 const BACKEND_URL = 'https://play-with-crypto-api.herokuapp.com'
-//const APP_DOMAIN = 'play-with-crypto.fr'
-const APP_DOMAIN = 'localhost'
+const APP_DOMAIN = 'play-with-crypto.fr'
 
 export { BACKEND_URL, APP_DOMAIN }
